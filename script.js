f788571b2639fe0387195d86c4687eff5a70cd20
SimoneSaramondi <simo98sara@gmail.com>
2021-04-29 22:17:21 +0200

    End project

---
+++ script.js
@@ -0,0 +1,11 @@
+const myFirstVue = new Vue(
+    {
+        el: '#vue',
+        data: {
+            myClass: "",
+            joke: {
+                avatar: 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190626123927/untitlsssssed.png'
+              }
+        },
+    }
+);
