d736d82cc277b4d54684a43c8e8862b831a87987
SimoneSaramondi <simo98sara@gmail.com>
2021-04-30 08:45:02 +0200

    Miglioramento codice

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const myFirstVue = new Vue(
         el: '#vue',
         data: {
             messagge:"Messaggio da stampare",
-            myClass: "",
             joke: {
                 avatar: 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190626123927/untitlsssssed.png'
               }
