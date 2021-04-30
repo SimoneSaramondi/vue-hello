07d97f06fe3ba63c5b190eee0b79c9956bb99900
SimoneSaramondi <simo98sara@gmail.com>
2021-04-30 08:40:15 +0200

    varie prove

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const myFirstVue = new Vue(
     {
         el: '#vue',
         data: {
+            messagge:"Messaggio da stampare",
             myClass: "",
             joke: {
                 avatar: 'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190626123927/untitlsssssed.png'
